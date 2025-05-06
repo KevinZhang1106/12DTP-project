4096adc465011959b2ce10681963628b56da499f
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-06 14:40:27 +1200

    created champion ranking page and foundation styling

--- home.py
+++ home.py
@@ -56,5 +56,31 @@ def championstatspage(champ_id, lane_id):
     return render_template("champions.html", championstats=championstats, searchbar=searchbar, available_lanes=available_lanes, all_lanes=all_lanes, current_lane=lane_id, champ_id=champ_id)
 
 
+@app.route('/championranking/<int:lane_id>')
+def championrankingpage(lane_id):
+    conn = sqlite3.connect("champions.db")
+    cur = conn.cursor()
+    cur.execute("SELECT cs.champ_id, c.champ_name, cs.winrate, cs.pickrate, cs.banrate FROM ChampionStats cs "
+                "JOIN Champions c ON cs.champ_id=c.champ_id "                
+                "WHERE cs.lane_id=? "
+                "ORDER BY cs.winrate DESC", (lane_id, )
+                )
+    ranking = cur.fetchall()
+
+    cur.execute("SELECT lane_id, lane_name FROM Lanes")
+    all_lanes = cur.fetchall()
+
+    cur.execute(
+        "SELECT cs.champ_id, cs.lane_id, c.champ_name, cs.pickrate FROM ChampionStats cs "
+        "JOIN (SELECT champ_id, MAX(pickrate) as max_pickrate FROM ChampionStats GROUP BY champ_id) max_stats "
+        "ON cs.champ_id=max_stats.champ_id JOIN Champions c ON cs.champ_id=c.champ_id "
+        "WHERE cs.pickrate=max_stats.max_pickrate"
+    )
+    searchbar = cur.fetchall()
+
+    conn.close()
+    return render_template("championranking.html", ranking=ranking, all_lanes=all_lanes, current_lane=lane_id, searchbar=searchbar)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
